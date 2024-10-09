41ef057d7530c42abc7ed088031ec0162f8adae2
karen-2024 <177392295+karen-2024@users.noreply.github.com>
2024-10-09 13:28:59 +0000

    [Add] Bootstrap導入・アクセス制限

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//require("@rails/ujs").start()
+//require("turbolinks").start()
+//require("@rails/activestorage").start()
+//require("channels")
