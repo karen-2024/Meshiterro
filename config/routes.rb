24784b90290000f341c9aaf0d7e74557b3c92263
karen-2024 <177392295+karen-2024@users.noreply.github.com when>
2024-11-02 08:21:09 +0000

    [Add]管理者ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root to: "homes#top"
   
   get '/homes/about' => 'homes#about', as: 'about'
+
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
