974f1561be540c8540160f9b8f93822f3ce0cba4
karen-2024 <177392295+karen-2024@users.noreply.github.com when>
2024-11-02 08:36:15 +0000

    [Add]管理者用のダッシュボード機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+ 
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
+  end
   
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorite, only: [:create, :destroy]
